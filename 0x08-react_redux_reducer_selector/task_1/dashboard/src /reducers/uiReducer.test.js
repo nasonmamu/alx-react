a652f4e87c331aa62b697e7543a2cd0ffb3e025f
nasonmamu <nasonmamu94@gmail.com>
2024-08-23 17:06:04 +0300

    Task

--- 0x08-react_redux_reducer_selector/task_1/dashboard/src /reducers/uiReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src /reducers/uiReducer.test.js
@@ -1,42 +1,43 @@
 import { initialState, uiReducer } from './uiReducers';
 import { SELECT_COURSE } from "../actions/courseActionTypes";
-import { LOGOUT, DISPLAY_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE } from '../actions/uiActionTypes';
-// Write a test verifying the state returned by the uiReducer function equals the initial state when no action is passed
-// Write a test verifying the state returned by the uiReducer function equals the initial state when the action SELECT_COURSE is passed
-// Write a test verifying the state returned by the uiReducer function, when the action DISPLAY_NOTIFICATION_DRAWER is passed, changes correctly the isNotificationDrawerVisible property
+import { LOGOUT, DISPLAY_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE } from '../actions/uiActionTypes'
+import { fromJS } from "immutable";
 
-const state = {
-    isNotificationDrawerVisible: false,
-    isUserLoggedIn: true,
-    user: {}
-}
+// You can use the toJS function within your tests for an easy comparison
+// Remember that Immutable.js always return a new Map after a modification
+
+const state = fromJS({
+  isNotificationDrawerVisible: false,
+  isUserLoggedIn: true,
+  user: {}
+})
 
 describe("test uiReducer function", () => {
   it("(uiReducer function) returns state equals the initial state when no action is passed", ()=>{
-    const currentState = uiReducer(initialState, {})
+    const currentState = uiReducer(undefined, {})
     expect(currentState).toEqual(initialState)
   })
 
   it("(uiReducer function) returns state equals the initial state when action SELECT_COURSE is passed", ()=>{
-    const currentState = uiReducer(initialState, { type: SELECT_COURSE })
+    const currentState = uiReducer(undefined, { type: SELECT_COURSE })
     expect(currentState).toEqual(initialState)
   })
 
   it("(uiReducer function) returns state equals the initial state when action DISPLAY_NOTIFICATION_DRAWER is passed", ()=>{
-    const currentState = uiReducer(initialState, { type: DISPLAY_NOTIFICATION_DRAWER })
+    const currentState = uiReducer(undefined, { type: DISPLAY_NOTIFICATION_DRAWER })
     const expectedState = { isNotificationDrawerVisible: true, isUserLoggedIn: false, user: {} }
-    expect(currentState).toEqual(expectedState)
+    expect(currentState.toJS()).toEqual(expectedState)
   })
 
   it(`returns state changes isUserLoggedIn property correctly when action LOGIN_FAILURE is passed`, ()=>{
     const currentState = uiReducer(state, { type: LOGIN_FAILURE })
     const expectedState = { isNotificationDrawerVisible: false, isUserLoggedIn: false, user: {} }
-    expect(currentState).toEqual(expectedState)
+    expect(currentState.toJS()).toEqual(expectedState)
   })
 
   it(`returns state changes isUserLoggedIn property correctly when action LOGOUT is passed`, () => {
     const currentState = uiReducer(state, { type: LOGOUT })
     const expectedState = { isNotificationDrawerVisible: false, isUserLoggedIn: false, user: {} }
-    expect(currentState).toEqual(expectedState)
+    expect(currentState.toJS()).toEqual(expectedState)
   })
 })
